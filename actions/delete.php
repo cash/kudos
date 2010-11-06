aea57003004f1dfb37a72eb3da9d13cc3e8db009
cash <cash.costello@gmail.com>
2010-11-05 22:28:04 -0400

    deleting kudos

--- actions/delete.php
+++ actions/delete.php
@@ -1,1 +1,19 @@
 <?php
+
+$annotation_id = get_input('annotation_id');
+
+$kudos = get_annotation($annotation_id);
+if (!$kudos) {
+	register_error(elgg_echo('kudos:delete:failure'));
+	forward(REFERER);
+}
+
+if (!$kudos->canEdit()) {
+	register_error(elgg_echo('kudos:delete:failure'));
+	forward(REFERER);
+}
+
+$kudos->delete();
+
+system_message(elgg_echo('kudos:delete:success'));
+forward(REFERER);
--- languages/en.php
+++ languages/en.php
@@ -5,6 +5,9 @@ $english = array(
 	'kudos:user:title' => "%s's kudos",
 
 	'kudos:given' => 'Given',
+
+	'kudos:delete:success' => 'Kudos deleted',
+	'kudos:delete:failure' => 'Kudos delete failed',
 	
 	'kudos:river:created' => '%s was given a kudos by the admins',
 
