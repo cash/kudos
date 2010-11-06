64e55d618cd2f4256c1361b2f484852df7ca4d3c
cash <cash.costello@gmail.com>
2010-11-05 22:22:30 -0400

    cleaned up the kudos annotation view

--- languages/en.php
+++ languages/en.php
@@ -2,7 +2,10 @@
 
 $english = array(
 	'kudos:hover:menu' => 'Give kudos',
+	'kudos:user:title' => "%s's kudos",
 
+	'kudos:given' => 'Given',
+	
 	'kudos:river:created' => '%s was given a kudos by the admins',
 
 );
--- views/default/kudos/list_item_info.php
+++ views/default/kudos/list_item_info.php
@@ -3,45 +3,23 @@
 $user = get_user($vars['annotation']->entity_guid);
 $poster = get_user($vars['annotation']->owner_guid);
 
-echo "<a href=\"{$user->getURL()}\">$user->name</a>";
+echo "<p><b><a href=\"{$user->getURL()}\">$user->name</a></b></p>";
+echo '<p>' . elgg_echo('kudos:given') . ' ' . elgg_view_friendly_time($vars['annotation']->time_created) . '</p>';
+echo '<br />';
+
 echo elgg_view("output/longtext", array("value" => $vars['annotation']->value));
 
 ?>
-
-<div class="generic_comment">
-	<div class="generic_comment_icon">
 <?php
-		echo elgg_view("profile/icon", array('entity' => $user, 'size' => 'small'));
+if ($vars['annotation']->canEdit()) {
 ?>
-	</div>
-	<div class="generic_comment_details">
-
-		<?php echo elgg_view("output/longtext", array("value" => $vars['annotation']->value)); ?>
-
-		<p class="generic_comment_owner">
-			<a href="<?php echo ''//$user->getURL(); ?>"><?php echo $user->name; ?></a> <?php echo elgg_view_friendly_time($vars['annotation']->time_created); ?>
-		</p>
-
-		<?php
-
-			// if the user looking at the comment can edit, show the delete link
-			if ($vars['annotation']->canEdit()) {
-
-			?>
-		<p>
-			<?php
-
-				echo elgg_view("output/confirmlink",array(
-					'href' => $vars['url'] . "action/comments/delete?annotation_id=" . $vars['annotation']->id,
-					'text' => elgg_echo('delete'),
-					'confirm' => elgg_echo('deleteconfirm'),
+<p>
+<?php
+	echo elgg_view("output/confirmlink", array(
+					'href' => $vars['url'] . "action/kudos/delete?annotation_id=" . $vars['annotation']->id,
+					'text' => elgg_echo('delete')
 				));
-
-			?>
-		</p>
-
-			<?php
-			} //end of can edit if statement
-		?>
-	</div>
-</div>
+?>
+</p>
+<?php
+}
